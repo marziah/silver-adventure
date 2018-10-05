28f99ea305dfc599223ee55e3c7528966ce7c480
marziah <marziah@gmail.com>
2018-10-04 19:24:39 -0700

    buggy return on button

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
     
-var count = 1;
+function handleSubmit(event) {
+    var form = event.target;
+    var elements = form.elements;
+    var comments = form.comments;
+    
 
-function sayHello() {
-    console.log('Hello world, for the ', count, 'time');
-    count++;
+    var name = elements.name.value;
+    console.log('name', name);
+
+    var comments = elements.comments.value;
+    console.log('comments', comments)
+
 }
- 
